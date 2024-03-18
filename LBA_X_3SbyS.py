ea4e0a3b93af57cfd386587e496050502405fb66
Tobia D. Carozzi <tobia@chalmers.se>
2024-03-18 23:56:13 +0100

    Changed to use get_antspats_arr() method

--- LBA_X_3SbyS.py
+++ LBA_X_3SbyS.py
@@ -4,8 +4,6 @@ from nec2 import (ArrayModel, VoltageSource, FreqSteps, Wire,
                   ExecutionBlock, RadPatternSpec)
 
 np.set_printoptions(precision=4, linewidth=80)
-PRINT_inpparm = False
-PRINT_radpat = True
 
 puck_width = 0.090
 puck_height = 1.6
@@ -57,39 +55,55 @@ print()
 sv = lba_model.calc_steering_vector(eb_arr)
 
 refantnr = 0
-for antnr in range(nr_ants):
-    eep = eepdat.eeps[antnr]
-    eel = eeldat.eels[antnr]
-    print("Antenna nr:", antnr)
-    for fidx, frq in enumerate(eep.freqs):
-        indent = '  '
-        print(indent+'Frequency:', frq)
-        #print(2*indent+'Radpats', eep.thetas.shape, eep.phis.shape)
-        if PRINT_inpparm:
-            print(2*indent+'Impedance', eep.inp_Z)
-            print(2*indent+'Current', eep.inp_I)
-            print(2*indent+'Voltage', eep.inp_V)
-        if PRINT_radpat:
-            #print('E_theta_amp', np.abs(eep.ef_tht[fidx]))
-            #print('E_theta_phs', np.rad2deg(np.angle(eep.ef_tht[fidx])))
-            print(2*indent+'Theta, Phi:')
-            #thetagrd , phigrd = np.meshgrid(eep.thetas, eep.phis,
-            #                                indexing='ij')
-            #print(thetagrd, phigrd)
-            print(2*indent+'E_phi_amp')
-            print(3*indent, str(np.abs(eep.f_phi[fidx])
-                                ).replace('\n', '\n'+3*indent))
-            print(2*indent+'E_phi_phs')
-            _ref_amphs = eepdat.eeps[refantnr].f_phi[fidx]
-            print(3*indent, str(np.angle(
-                eep.f_phi[fidx]*np.conj(_ref_amphs), deg=True)
-                                ).replace('\n', '\n'+3*indent))
-            #
-            print(2*indent+'eff. lengths')
-            print(3*indent, str(np.abs(eel.f_phi[fidx])
-                                ).replace('\n', '\n'+3*indent) )
-            print(2*indent+'steering_phs')
-            print(3*indent, str(np.angle(sv[antnr, fidx], deg=True)
-                                ).replace('\n', '\n'+3*indent))
+if False:
+    PRINT_inpparm = False
+    PRINT_radpat = True
+    for antnr in range(nr_ants):
+        eep = eepdat.eeps[antnr]
+        eel = eeldat.eels[antnr]
+        print("Antenna nr:", antnr)
+        for fidx, frq in enumerate(eep.freqs):
+            indent = '  '
+            print(indent+'Frequency:', frq)
+            #print(2*indent+'Radpats', eep.thetas.shape, eep.phis.shape)
+            if PRINT_inpparm:
+                print(2*indent+'Impedance', eep.inp_Z)
+                print(2*indent+'Current', eep.inp_I)
+                print(2*indent+'Voltage', eep.inp_V)
+            if PRINT_radpat:
+                #print('E_theta_amp', np.abs(eep.ef_tht[fidx]))
+                #print('E_theta_phs', np.rad2deg(np.angle(eep.ef_tht[fidx])))
+                print(2*indent+'Theta, Phi:')
+                #thetagrd , phigrd = np.meshgrid(eep.thetas, eep.phis,
+                #                                indexing='ij')
+                #print(thetagrd, phigrd)
+                print(2*indent+'E_phi_amp')
+                print(3*indent, str(np.abs(eep.f_phi[fidx])
+                                    ).replace('\n', '\n'+3*indent))
+                print(2*indent+'E_phi_phs')
+                _ref_amphs = eepdat.eeps[refantnr].f_phi[fidx]
+                print(3*indent, str(np.angle(
+                    eep.f_phi[fidx]*np.conj(_ref_amphs), deg=True)
+                                    ).replace('\n', '\n'+3*indent))
+                #
+                print(2*indent+'eff. lengths')
+                print(3*indent, str(np.abs(eel.f_phi[fidx])
+                                    ).replace('\n', '\n'+3*indent) )
+                print(2*indent+'steering_phs')
+                print(3*indent, str(np.angle(sv[antnr, fidx], deg=True)
+                                    ).replace('\n', '\n'+3*indent))
 
-    print()
+ant_h = eeldat.get_antspats_arr()
+pol_def = {'tht': 0, 'phi': 1}
+pol_sel = 'phi'  # phi is along antenna alignment
+print('For pol component:', pol_sel)
+for fidx, frq in enumerate(eeldat.eels[0].freqs):
+    h_mat = ant_h[:,fidx,:,:,pol_def[pol_sel]].reshape((ant_h.shape[0], -1))
+    print('Gain (Ant,Direction) matrix:')
+    print(h_mat*np.conj(h_mat[refantnr, 0]))
+    sv_mat = sv[:,fidx,:,:].reshape((sv.shape[0],-1))
+    print('Steering (Ant,Direction) matrix:')
+    print(sv_mat)
+    print('Gain Cal mat')
+    print(sv_mat @ np.linalg.inv(h_mat))
+
